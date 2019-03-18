cb30d02b1e5761b375e607419a7f79512f3a968a
bastien <bastien.nicoud@gmail.com>
2019-03-18 15:46:08 +0100

    Add Question model

---
+++ lib/quizz/Question.dart
@@ -0,0 +1,22 @@
+class Question {
+
+  String caption;
+  List<String> answers;
+  int correctAnswer;
+  String hint;
+
+  Question({
+    this.caption,
+    this.answers,
+    this.correctAnswer,
+    this.hint
+  });
+
+  factory Question.fromJson(Map<String, dynamic> json) => new Question(
+    caption: json['caption'],
+    answers: new List<String>.from(json['answers']),
+    correctAnswer: json['correct_answer_index'],
+    hint: json['hint']
+  );
+
+}
