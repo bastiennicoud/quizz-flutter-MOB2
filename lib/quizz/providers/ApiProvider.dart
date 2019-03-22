e7dd080597cbc3f24c994ff4af450706c4c675d7
bastien <bastien.nicoud@gmail.com>
2019-03-22 09:01:32 +0100

    Add api and local question provider

--- lib/quizz/providers/ApiProvider.dart
+++ lib/quizz/providers/ApiProvider.dart
@@ -1,5 +1,21 @@
+import 'dart:convert';
+
+import 'package:test_flutter/quizz/Question.dart';
 import 'package:test_flutter/quizz/providers/Provider.dart';
+import 'package:http/http.dart' as http;
 
 class ApiProvider extends Provider {
+
+  String endpoint;
+
+  ApiProvider({
+    this.endpoint: 'http://172.20.10.3:4567/questions/next'
+  });
+
+  Future<Question> getNextQuestion() async {
+    var response = await http.get(endpoint);
+    var data = json.decode(response.body);
+    return Question.fromJson(data);
+  }
 
 }
--- lib/quizz/providers/LocalProvider.dart
+++ lib/quizz/providers/LocalProvider.dart
@@ -3,6 +3,8 @@ import 'package:test_flutter/quizz/providers/Provider.dart';
 
 class LocalProvider extends Provider {
 
+  String endpoint;
+
   Question getNextQuestion() async {
     // Http call
     // Build the question object
